71ffecdbcef0b0961e64d0034ff6ebd221dcbcd6
Zohratul Zannat <zzannat@my.centennialcollege.ca>
2024-03-16 03:07:58 -0400

    Add ASP.NET web application files

---
+++ ASP .NET WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.NET_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
